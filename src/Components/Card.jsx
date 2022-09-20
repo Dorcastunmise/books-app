c8d4fe460504b185a0e033909b892ac7b32cdd53
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-09-20 10:44:35 +0100

    pictures issue sorted

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,19 +2,18 @@ import React, { useState } from "react";
 import Modal from "./Modal";
 const Card = ({ book }) => {
 
-    const [show,setShow]=useState(false);
-    const [bookItem,setItem]=useState();
+    const [show,setShow] = useState(false);
+    const [bookItem,setItem] = useState();
     console.log(book)
     return (
         <>
             {
-                book.map((item) => {
+                book.map((item, index) => {
                     let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
                     let amount=item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-                    if(thumbnail!= undefined && amount !=undefined)
-                    {
+                    
                         return (
-                            <>
+                            <div key={"book_" + index}>
                             <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
                                 <img src={thumbnail} alt="" />
                                 <div className="bottom">
@@ -23,9 +22,9 @@ const Card = ({ book }) => {
                                 </div>
                             </div>
                               <Modal show={show} item={bookItem} onClose={()=>setShow(false)}/>
-                            </>
+                            </div>
                         )
-                    }
+                    
                     
                 })
             }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,16 +3,14 @@ import Card from "./Card";
 import axios from "axios";
 import photo from "../images/rain-solo-time.gif"
 
-const Main=()=>{
-    const [search,setSearch]=useState("");
-    const [bookData,setData]=useState([]);
-    const searchBook=(evt)=>{
-        if(evt.key==="Enter")
-        {
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
-            .then(res=>setData(res.data.items))
-            .catch(err=>console.log(err))
-        }
+const Main = () => {
+    const [search, setSearch]=useState("");
+    const [bookData, setData]=useState([]);
+    const searchBook=()=>{
+        
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyBI7w9JfGzUp8CZg22K-DRgI_RVqYlnteI'+'&maxResults=40')
+            .then(res => setData(res.data.items))
+            .catch(err => console.log(err))
     }
     return(
         <>
@@ -23,9 +21,13 @@ const Main=()=>{
                 <div className="row2">
                     <h2>Find Your Book</h2>
                     <div className="search">
-                        <input type="text" placeholder="Enter Your Book Name"
-                        value={search} onChange={e=>setSearch(e.target.value)}
-                        onKeyPress={searchBook}/>
+                        <input 
+                        type="text" 
+                        placeholder="Enter Your Book Name"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                        onKeyPress={searchBook}
+                        />
                         <button><i className="fas fa-search"></i></button>
                     </div>
                     <img src={photo} alt="" />
